ed579bb6c534317cf03a9697b2d2512c1cc676b4
Miryam-Choor <M400miryam@gmail.com>
2026-08-07 12:05:23 +0300

    add to server: Handling client requests, printing request data, and using middlewares in the project

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 // app.js
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import mainRouter from './routes/index.route.js';
 
 const app = express();
 
 app.use(express.json());
+
+// מאפשר גישה מכל פרויקט client ומטפל בבעיות CORS
+app.use(cors());
+
+// אבטחת השרת על ידי הוספת HTTP headers מתאימים
+app.use(helmet());
+
+// הגבלת מספר הבקשות מכתובת IP מסוימת למניעת מתקפות Brute Force או העמסת יתר
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 דקות
+    max: 100 // הגבלה ל-100 בקשות לכל IP
+});
+app.use(limiter);
+
+// הדפסת נתוני בקשות בקונסול במצב פיתוח בלבד
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+}
 
 app.use('/api', mainRouter);
 
